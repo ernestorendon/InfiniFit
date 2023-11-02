7ad9dbc870e3455324e6281f5840c4c6bc8599b4
Ernesto <ernestorendon@ufl.edu>
2023-11-02 19:44:36 -0400

    Add page and prelim functionality for user registration

--- app/src/App.js
+++ app/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Registration from './Registration';
+import RegistrationForm from './RegistrationForm';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Registration />} />
+        <Route path="/register" element={<RegistrationForm />} />
       </Routes>
     </Router>
   );
---
+++ app/src/RegistrationForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import './RegistrationForm.css'; // Import the CSS file
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    firstName: '',
+    lastName: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      alert('Password and confirmation do not match.');
+    } else {
+      console.log('Form data submitted:', formData);
+    }
+  };
+
+  return (
+    <div className="registration-form">
+      {/* <h1>Registration Form</h1> */}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>First Name:</label>
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password Confirmation:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
