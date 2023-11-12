7c8d509fcce48ee83d0f29024a85bd12d9065cf1
Rohit Mittal <rohitmit98@gmail.com>
2023-11-11 23:03:25 -0500

    store local auth token, refactored Navbar component, added logout button

--- app/src/AuthContext.js
+++ app/src/AuthContext.js
@@ -1,16 +1,33 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext(null);
 
-export default AuthContext;
-
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // Example state to track the user
+  // Initialize the user state from localStorage
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  // Update localStorage when the user state changes
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      localStorage.removeItem('user');
+    }
+  }, [user]);
+
+  // Provide a function to log out the user
+  const logout = () => {
+    setUser(null); // This will also trigger the useEffect to remove user from localStorage
+  };
 
   // The value that will be given to the context
   const contextValue = {
     user,
-    setUser
+    setUser,
+    logout
   };
 
   return (
@@ -19,3 +36,5 @@ export const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+export default AuthContext;
