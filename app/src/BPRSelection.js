2f9c8ad859bf9d26db9d4529a9fe8b3a00fb808c
Mary Klawa <100811655+mklawa@users.noreply.github.com>
2023-12-05 15:08:03 -0500

    fixed apiURLs, runtime in custom_routine, string-to-int counter

--- app/src/BPRSelection.js
+++ app/src/BPRSelection.js
@@ -37,24 +37,28 @@ const selectRoutine = (routineId) => {
 };
 
 
-  const routines = {
-    1: {
+  const routines = [
+    {
+      id: 1,
       name: 'Full Body Workout',
       description: 'A dynamic routine targeting all major muscle groups with air squats, pushups, situps, walking lunges, and mountain climbers.'
     },
-    2: {
+    {
+      id: 2,
       name: 'Core Routine',
       description: 'Strengthen your core with a focused series of crunches, flutter kicks, situps, leg raises, and planks.'
     },
-    3: {
+    {
+      id: 3,
       name: 'Upper Body Routine',
       description: 'Build upper body strength with pushups, bench dips, pike pushups, and supermans.'
     },
-    4: {
+    {
+      id: 4,
       name: 'Lower Body Routine',
       description: 'Tone and strengthen your lower body with air squats, calf raises, walking lunges, lying glute bridges, and jump squats.'
     }
-  };
+  ];
 
 
   return (
@@ -62,10 +66,10 @@ const selectRoutine = (routineId) => {
       <Navbar/>
 
       <section className="info">
-        {[1, 2, 3, 4].map((routineNum) => (
-          <div key={routineNum} className="info-block" onClick={() => selectRoutine(routineNum)}>
-            <h3>{routines[routineNum].name}</h3>
-            <p>{routines[routineNum].description}</p>
+        {routines.map((routine) => (
+          <div key={routine.id} className="info-block" onClick={() => selectRoutine(routine.id)}>
+            <h3>{routine.name}</h3>
+            <p>{routine.description}</p>
           </div>
         ))}
       </section>
